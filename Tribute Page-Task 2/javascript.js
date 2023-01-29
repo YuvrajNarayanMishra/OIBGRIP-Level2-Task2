d2c57d7162739191ec36220978f58a7eef04eede
Yuvraj Narayan Mishra <vn9451514260@gmail.com>
2023-01-29 18:12:42 +0530

    Add files via upload

---
+++ Tribute Page-Task 2/javascript.js
@@ -0,0 +1,8 @@
+
+// function for toggling dark mode on the website with the click of a button
+
+function darkMode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+
+}
